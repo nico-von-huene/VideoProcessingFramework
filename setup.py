430911ed19a88c11a0620f53fc98204333bf52d0
Nico v. Huene <nico.von.huene@kinexon.com>
2025-01-22 14:21:40 +0100

    torch/vision deps only in PytorchNvCodec

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ if __name__ == "__main__":
         install_requires=["numpy==1.24.4", "wheel", "setuptools", "cmake", "scikit-build"],
         extras_require={
             # , "PyOpenGL-accelerate" # does not compile on 3.10
-            # "dev": ["pycuda", "pyopengl", "torch", "torchvision", "opencv-python", "onnx", "tensorrt", f"PytorchNvCodec @ file://{os.getcwd()}/src/PytorchNvCodec/"],
-            "samples": ["pycuda", "pyopengl", "torch>=2.2.0a0,<2.2.1", "torchvision>=0.17.0a0,<0.17.1", "opencv-python", "onnx", "tensorrt", "tqdm", PytorchNvCodec],
-            "tests": ["pycuda", "pyopengl", "torch>=2.2.0a0,<2.2.1", "torchvision>=0.17.0a0,<0.17.1", "opencv-python", PytorchNvCodec],
-            "torch": ["torch>=2.2.0a0,<2.2.1", "torchvision>=0.17.0a0,<0.17.1", "numpy==1.24.4", PytorchNvCodec],
-            "tensorrt": ["torch>=2.2.0a0,<2.2.1", "torchvision>=0.17.0a0,<0.17.1", "numpy==1.24.4", PytorchNvCodec],
+            # "dev": ["pycuda", "pyopengl", "opencv-python", "onnx", "tensorrt", f"PytorchNvCodec @ file://{os.getcwd()}/src/PytorchNvCodec/"],
+            "samples": ["pycuda", "pyopengl", "opencv-python", "onnx", "tensorrt", "tqdm", PytorchNvCodec],
+            "tests": ["pycuda", "pyopengl", "opencv-python", PytorchNvCodec],
+            "torch": [PytorchNvCodec],
+            "tensorrt": ["onnx", "tensorrt", PytorchNvCodec],
         },
         dependency_links=[
             "https://pypi.ngc.nvidia.com"
--- src/PytorchNvCodec/setup.py
+++ src/PytorchNvCodec/setup.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 
     setup(
         name="PytorchNvCodec",
-        install_requires=["torch>=2.2.0a0,<2.2.1", "numpy==1.24.4", "wheel", "cmake"],
+        install_requires=["torch>=2.2.0a0,<2.2.1", "torchvision>=0.17.0a0,<0.17.1", "numpy==1.24.4", "wheel", "cmake>=3.21", "ninja; platform_system!='Windows'", "pkgconfig"],
         ext_modules=[CUDAExtension("_PytorchNvCodec", ["src/PytorchNvCodec.cpp"])],
         packages=["PytorchNvCodec"],
         cmdclass={"build_ext": BuildExtension},
