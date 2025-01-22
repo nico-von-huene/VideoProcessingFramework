9bc1f9e7746b24ea963027b9b9f401c5fcdd82f3
Nico v. Huene <nico.von.huene@kinexon.com>
2025-01-22 14:49:28 +0100

    using skbuild for PytorchNvCodec wheel

--- src/PytorchNvCodec/setup.py
+++ src/PytorchNvCodec/setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from skbuild import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 
@@ -7,6 +7,7 @@ if __name__ == "__main__":
     setup(
         name="PytorchNvCodec",
         install_requires=["torch>=2.2.0a0,<2.2.1", "torchvision>=0.17.0a0,<0.17.1", "numpy==1.24.4", "wheel", "cmake>=3.21", "ninja; platform_system!='Windows'", "pkgconfig"],
+        python_requires=">=3.10",
         ext_modules=[CUDAExtension("_PytorchNvCodec", ["src/PytorchNvCodec.cpp"])],
         packages=["PytorchNvCodec"],
         cmdclass={"build_ext": BuildExtension},
