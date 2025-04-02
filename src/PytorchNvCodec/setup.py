b84acc19634d9afe5b857750a1514f2977182d99
Nico v. Huene <nico.von.huene@kinexon.com>
2025-04-02 23:40:27 +0200

    torch1.11.0+cu115

--- src/PytorchNvCodec/setup.py
+++ src/PytorchNvCodec/setup.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 
     setup(
         name="PytorchNvCodec",
-        install_requires=["torch"],
+        install_requires=["torch>1.10,<1.12", "numpy==1.22.4"],
         ext_modules=[CUDAExtension("_PytorchNvCodec", ["src/PytorchNvCodec.cpp"])],
         packages=["PytorchNvCodec"],
         cmdclass={"build_ext": BuildExtension},
